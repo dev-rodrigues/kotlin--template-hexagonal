15b6fd7a963e7d2d94ab0f349cddf2f58e54c9e4
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2023-04-13 22:02:34 -0300

    init

---
+++ src/main/kotlin/br/com/devrodrigues/chat/KotlinHexagonalTemplateApplication.kt
@@ -0,0 +1,11 @@
+package br.com.devrodrigues.chat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinHexagonalTemplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinHexagonalTemplateApplication>(*args)
+}
